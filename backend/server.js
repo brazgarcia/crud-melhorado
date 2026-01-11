ee77f0c850e3420b6c8ef217c6356e89062c80ae
Braz da Silva Garcia <bdanielgarcia@hotmail.com>
2026-01-11 12:21:01 -0300

    Adicionando produtos no backend

--- backend/server.js
+++ backend/server.js
@@ -6,48 +6,73 @@ app.use(cors());
 app.use(express.json());
 
 let users = [];
-let emails = [];
-let telefones = [];
-let cidades = [];
-let idCounter = 1;
+let idCounterUser = 1;
 
-// Create
-app.post('/users', (req, res) => {
-    const user = {
-        id: idCounter++,
-        nome: req.body.nome,
-        email: req.body.email,
-        telefone: req.body.telefone,
-        cidade: req.body.cidade
-    };
-    users.push(user);
-    res.status(201).json(user);
-});
+let products = [];
+let idCounterProducts = 1;
 
-// Read
-app.get('/users', (req, res) => {
-    res.json(users);
-});
+// Users
+    // Create
+    app.post('/users', (req, res) => {
+        const user = {
+            id: idCounterUser++,
+            nome: req.body.nome,
+            email: req.body.email,
+            telefone: req.body.telefone,
+            cidade: req.body.cidade
+        };
+        users.push(user);
+        res.status(201).json(user);
+    });
 
-// Update
-app.put('/users/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const user = (u => u.id === id);
+    // Read
+    app.get('/users', (req, res) => {
+        res.json(users);
+    });
 
-    if(!user) return res.status(404).json({Error: "Usuário não encontrado" });
+    // Update
+    app.put('/users/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        const user = (u => u.id === id);
 
-    user.nome = req.body.nome;
-    user.email = req.body.email;
-    user.telefone = req.body.telefone;
-    user.cidade = req.bosy.cidade;
-    res.json(users);
-});
+        if(!user) return res.status(404).json({Error: "Usuário não encontrado" });
 
-// Delete
-app.delete('/users/:id', (req, res) => {
-    users = users.filter(u => u.id !== parseInt(res.params.id));
-    res.status(204).send();
-});
+        user.nome = req.body.nome;
+        user.email = req.body.email;
+        user.telefone = req.body.telefone;
+        user.cidade = req.bosy.cidade;
+        res.json(users);
+    });
+
+    // Delete
+    app.delete('/users/:id', (req, res) => {
+        users = users.filter(u => u.id !== parseInt(res.params.id));
+        res.status(204).send();
+    });
+
+// Products
+    // Creat
+    app.post('/products', (req, res) => {
+        product = {
+            id: idCounterProducts++, 
+            nomeProduto: req.params.nome-produto,
+            codBarra: req.params.codBarras,
+            descricao: req.params.descricao,
+            categoria: req.params.categoria
+        };
+        products.push(product);
+        app.status(201).json();
+    });
+
+    // Read
+    app.get('/produtcs', (req, res) => {
+        res.json(products);
+    });
+
+    // Update
+    app.put('/products/:id', (req, res) => {
+        
+    })
 
 app.listen(3000, () => {
     console.log("Servidor rodando no link http://localhost:3000");
