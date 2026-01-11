cc68cddd154d7ae4701607bc3be74c1a13e5205d
Braz da Silva Garcia <bdanielgarcia@hotmail.com>
2026-01-11 11:45:48 -0300

    Criando um CRUD com melhorias

---
+++ backend/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let users = [];
+let emails = [];
+let telefones = [];
+let cidades = [];
+let idCounter = 1;
+
+// Create
+app.post('/users', (req, res) => {
+    const user = {
+        id: idCounter++,
+        nome: req.body.nome,
+        email: req.body.email,
+        telefone: req.body.telefone,
+        cidade: req.body.cidade
+    };
+    users.push(user);
+    res.status(201).json(user);
+});
+
+// Read
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+// Update
+app.put('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const user = (u => u.id === id);
+
+    if(!user) return res.status(404).json({Error: "Usuário não encontrado" });
+
+    user.nome = req.body.nome;
+    user.email = req.body.email;
+    user.telefone = req.body.telefone;
+    user.cidade = req.bosy.cidade;
+    res.json(users);
+});
+
+// Delete
+app.delete('/users/:id', (req, res) => {
+    users = users.filter(u => u.id !== parseInt(res.params.id));
+    res.status(204).send();
+});
+
+app.listen(3000, () => {
+    console.log("Servidor rodando no link http://localhost:3000");
+})
+
+
