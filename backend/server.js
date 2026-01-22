30fa2e3f7dfc7034e8bff003c4fdeeebaccd8d4e
Braz da Silva Garcia <bdanielgarcia@hotmail.com>
2026-01-21 21:23:22 -0300

    Mudança de projeto

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let users = [];
-let idCounterUser = 1;
+const port = 3000;
 
-let products = [];
-let idCounterProduct = 1;
+let users = [];
+let idCounter = 1;
 
 // Users
     // Create
     app.post('/users', (req, res) => {
         const user = {
-            id: idCounterUser++,
+            id: idCounter++,
             nome: req.body.nome,
             email: req.body.email,
             telefone: req.body.telefone,
@@ -50,48 +49,9 @@ let idCounterProduct = 1;
         res.status(204).send();
     });
 
-// Products
-    // Creat
-    app.post('/products', (req, res) => {
-        const product = {
-            id: idCounterProduct++, 
-            nomeProduto: req.body.nomeProduto,
-            codBarras: req.body.codBarras,
-            descricao: req.body.descricao,
-            categoria: req.body.categoria
-        };
-        products.push(product);
-        res.status(201).json(product);
-    });
-
-    // Read
-    app.get('/products', (req, res) => {
-        res.json(products);
-    });
-
-    // Update
-    app.put('/products/:id', (req, res) => {
-        const id = parseInt(req.params.id);
-        const product = products.find(u => u.id === id);
-
-        if(!product) return res.status(404).json({ error: "Produto não encontrado" });
-
-        product.nomeProduto = req.body.nomeProduto;
-        product.codBarras = req.body.codBarras;
-        product.descricao = req.body.descricao;
-        product.categoria = req.body.categoria;
-        res.json(product);
-    });
-
-    // Delete
-    app.delete('/products/:id', (req, res) => {
-        products = products.filter(u => u.id !== parseInt(req.params.id));
-        res.status(204).send();
-    });
-
 // Startar servidor
-app.listen(3000, () => {
-    console.log("Servidor rodando no link http://localhost:3000");
+app.listen(port, () => {
+    console.log(`Servidor rodando no link http://localhost:${port}`);
 })
 
 
--- frontend/script.js
+++ frontend/script.js
@@ -3,3 +3,15 @@ const API_URL = 'http://localhost:3000/users';
 const form = document.getElementById('userForm');
 const list = document.getElementById('usersList');
 
+form.addEventListeners('submit', async(e) => {
+    e.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const telefone = document.getElementById('telefone').value;
+    const cidade = document.getElementById('cidade').value;
+
+    await fetch ()
+});
+
+ 
