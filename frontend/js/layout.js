74c7b82e43166b6c840238120a839c85f56fb66e
brazgarcia <bdanielgarcia@hotmail.com>
2026-02-11 13:52:21 -0300

    Mudando arquitetura do diretório

--- frontend/js/layout.js
+++ frontend/js/layout.js
@@ -1,8 +1,8 @@
 async function loadLayout() {
-  const header = await fetch('layout/header.html');
+  const header = await fetch('../layout/header.html');
   document.getElementById('header').innerHTML = await header.text();
 
-  const footer = await fetch('layout/footer.html');
+  const footer = await fetch('../layout/footer.html');
   document.getElementById('footer').innerHTML = await footer.text();
 }
 
